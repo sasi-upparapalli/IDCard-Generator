b5beb97d7dcc41546fd0c5e0eee5cabdbeadec80
sasi-upparapalli <sasikanthupparapalli@gmail.com>
2025-12-12 18:09:13 +0530

    WIP: set homepage top-level and switch to HashRouter for GH Pages

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,3 @@
-// src/index.js  (also paste into src/index.jsx)
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { HashRouter } from 'react-router-dom'
@@ -6,16 +5,13 @@ import App from './App'
 import './styles/idcss.css'
 import './styles/idcard.css'
 
-// If you have index.css from CRA, you can keep it too (optional)
-// import './index.css'
-
 const container = document.getElementById('root')
 const root = createRoot(container)
 
 root.render(
   <React.StrictMode>
-<HashRouter>
-  <App />
-</HashRouter>
+    <HashRouter>
+      <App />
+    </HashRouter>
   </React.StrictMode>
 )
